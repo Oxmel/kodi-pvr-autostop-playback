68b1d2a47e84c3229c2a1a4bfa0087574939cdba
Oxmel <oxm3l@hotmail.com>
2016-10-27 03:07:57 +0200

    Better use cron timer :]

--- autostop-playback.py
+++ autostop-playback.py
@@ -5,17 +5,13 @@ import subprocess
 import shlex
 import httplib
 import json
-import time
 
 # Tv status code returned by 'tvservice'. May vary depending on the brand
 # Run 'tvservice -s' with TV turned On and Off to get your own
 On = "0x12000a" 
 Off = "0x120009" 
-
 # Connect to kodi web api. Kodi control over HTTP must be activated
-# Change the host IP and port according to your setup
 url = "localhost:8080"
-
 # Declare Headers
 headers = {'Content-Type': 'application/json'}
 
@@ -24,6 +20,7 @@ def tv():
     tvservice = subprocess.check_output(['tvservice', '-s'])
     # Split the output using shell like syntax. Much easier to parse
     tvStatus = shlex.split(tvservice)[1]
+
     if tvStatus == On :
         pass
     elif tvStatus == Off :
@@ -31,30 +28,22 @@ def tv():
 
 def player():
     # Request the list of all active players
-    playerStatus = {'jsonrpc': '2.0', 'method': 'Player.GetActivePlayers', 'id': '1'}
-
+    params = {'jsonrpc': '2.0', 'method': 'Player.GetActivePlayers', 'id': '1'}
     # Check for active players
     conn = httplib.HTTPConnection(url)
-    conn.request('POST', '/jsonrpc?', json.dumps(playerStatus), headers)
+    conn.request('POST', '/jsonrpc?', json.dumps(params), headers)
     response = conn.getresponse()
     playerStatus = json.loads(response.read())
 
     if playerStatus["result"] == [] :
         conn.close()
         pass
-
     else :
         # Get active player's id
         playerId = playerStatus["result"][0]["playerid"] 
         # Stop active player based on its id
-        stopCmd = {'jsonrpc':'2.0', 
-                'method':'Player.Stop', 
-                'id':1,
-                'params':{'playerid': playerId}}
+        stopCmd = {'jsonrpc':'2.0', 'method':'Player.Stop', 'id':1, 'params':{'playerid': playerId}}
         conn.request('POST', '/jsonrpc?', json.dumps(stopCmd), headers)
         conn.close()
 
-while True:
-    tv()
-    # Sleep 5min between each check
-    time.sleep(300)
+tv()
